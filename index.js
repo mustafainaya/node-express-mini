817cb627d1952f5eed713f4438453db2343dcccd
mustafa inaya <moose.inaya@icloud.com>
2019-03-13 21:46:15 -0500

    GETTING USERS.

--- index.js
+++ index.js
@@ -2,8 +2,29 @@
 console.log('Hello');
 const express = require('express');
 
+const db = require('./data/db');
 const server = express();
+const PORT = 3000;
 
-server.listen(3000, () => {
-	console.log('SERVER IS UP!');
+//endpoints
+server.get('/api/users', (req, res) => {
+	db
+		.find()
+		.then((users) => {
+			console.log('USERS', users);
+			res.json(users);
+		})
+		.catch((err) => {
+			res.status(500).json({ message: 'ERROR' });
+		});
+});
+
+server.get('/api/users/:id', (req, res) => {
+	db.findById().then().catch();
+});
+
+//Listining
+
+server.listen(PORT, () => {
+	console.log(`SERVER IS UP ON PORT ${PORT}!`);
 });
