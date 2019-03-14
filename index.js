a923e4815d5cc755f286447e03908466a812c4c8
mustafa inaya <moose.inaya@icloud.com>
2019-03-13 22:51:12 -0500

    UPDATE.

--- index.js
+++ index.js
@@ -34,7 +34,40 @@ server.get('/api/users/:id', (req, res) => {
 			res.status(500).json({ message: 'ERROR' });
 		});
 });
+server.put('/api/users/:id', (req, res) => {
+	const { id } = req.params;
+	const { name, bio } = req.body;
 
+	if (!name || !bio) {
+		res.status(400).json({ errorMessage: 'Please provide name and bio for the user.' });
+		return;
+	}
+
+	db
+		.update(id, { name, bio })
+		.then((user) => {
+			if (user) {
+				res.sendStatus(200).json(user);
+			} else {
+				res.status(400).json({ errorMessage: 'No users with this id exists.. ' });
+			}
+		})
+		.catch(() => {
+			res.status(500).json({ error: 'The user information could not be modified' });
+		});
+});
+
+server.delete('/api/users/:id', (req, res) => {
+	const { id } = req.params;
+	db
+		.remove(id)
+		.then((user) => {
+			res.status(404).json({ message: 'The user with the specified ID does not exist.' });
+		})
+		.catch((err) => {
+			res.status(500).json({ message: 'the user could not be removed' });
+		});
+});
 //Listining
 
 server.listen(PORT, () => {
