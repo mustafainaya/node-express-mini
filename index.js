f5ce4a8d53d4a28886dbe3157c7704fa34c19615
mustafa inaya <moose.inaya@icloud.com>
2019-03-13 20:56:35 -0500

    IMPLEMENT, Implemented Express And Node.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // implement your API here
+console.log('Hello');
+const express = require('express');
+
+const server = express();
+
+server.listen(3000, () => {
+	console.log('SERVER IS UP!');
+});
